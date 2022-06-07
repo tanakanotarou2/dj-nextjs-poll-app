f41de02c9d188e37ade2f979b0fdeeaa3dd7195b
tanakanotarou2 <sotaime+gh@gmail.com>
2022-06-08 03:55:17 +0900

    view の汎用メソッドを追加

--- backend/src/polls/views.py
+++ backend/src/polls/views.py
@@ -1,10 +1,9 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework import status, viewsets
-from rest_framework.mixins import CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, ListModelMixin
+from rest_framework import status
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
-from rest_framework.viewsets import GenericViewSet
 
+from lib.views import ModelViewSet
 from polls.models import Question
 from polls.serializers import QuestionDetailSerializer, QuestionSerializer, QuestionEditSerializer
 from polls.use_cases.actions import (
@@ -13,10 +12,12 @@ from polls.use_cases.actions import (
     QuestionUpdateAction,
 )
 
+
 class UpdateModelMixin:
     """
     Update a model instance.
     """
+
     def _update(self, request, *args, **kwargs):
         partial = kwargs.pop('partial', False)
         instance = self.get_object()
@@ -38,14 +39,8 @@ class UpdateModelMixin:
         kwargs['partial'] = True
         return self._update(request, *args, **kwargs)
 
-class QuestionViewSet(
-    CreateModelMixin,
-    RetrieveModelMixin,
-    UpdateModelMixin,
-    DestroyModelMixin,
-    ListModelMixin,
-    GenericViewSet
-):
+
+class QuestionViewSet(ModelViewSet):
     queryset = Question.objects.all()
     serializer_class = QuestionDetailSerializer
     permission_classes = [AllowAny]
@@ -66,18 +61,13 @@ class QuestionViewSet(
 
         self.get_serializer(res_instance)
 
-        # TODO: この考慮は UseCase でやるほうが適切か
+        # TODO: この考慮は UseCase でやるほうが適切と思う
         # if getattr(instance, '_prefetched_objects_cache', None):
         #     # If 'prefetch_related' has been applied to a queryset, we need to
         #     # forcibly invalidate the prefetch cache on the instance.
         #     instance._prefetched_objects_cache = {}
 
         return Response(serializer.data)
-
-    def wrap_success_create_response(self, response_data):
-        """create メソッドのレスポンスを構築する"""
-        headers = self.get_success_headers(response_data)
-        return Response(response_data, status=status.HTTP_201_CREATED, headers=headers)
 
     # inputとoutputのserializerが異なる場合、
     # ドキュメントの response の Serializer を指定する
